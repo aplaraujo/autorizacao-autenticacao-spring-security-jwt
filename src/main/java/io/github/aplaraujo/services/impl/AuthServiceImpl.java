1b830e2779e086df8087f9ca16b67d14bf9c4114
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-18 19:59:49 -0300

    JWT and security configuration part 2

--- src/main/java/io/github/aplaraujo/services/impl/AuthServiceImpl.java
+++ src/main/java/io/github/aplaraujo/services/impl/AuthServiceImpl.java
@@ -3,19 +3,24 @@ package io.github.aplaraujo.services.impl;
 import io.github.aplaraujo.entities.User;
 import io.github.aplaraujo.repositories.UserRepository;
 import io.github.aplaraujo.services.models.dto.LoginDTO;
+import io.github.aplaraujo.services.models.dto.ResponseDTO;
 import io.github.aplaraujo.services.models.validators.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
+@Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements IAuthService {
     private final UserRepository userRepository;
     private final IJWTUtilityService jwtUtilityService;
     private final UserValidator userValidator;
 
+    @Override
     public HashMap<String, String> login(LoginDTO dto) throws Exception {
         try {
             HashMap<String, String> jwt = new HashMap<>();
@@ -34,6 +39,35 @@ public class AuthServiceImpl implements IAuthService {
             }
 
             return jwt;
+
+        } catch (Exception e) {
+            throw new Exception(e.toString());
+        }
+    }
+
+    @Override
+    public ResponseDTO register(User user) throws Exception {
+        try {
+            ResponseDTO response = userValidator.validate(user);
+
+            if (response.getNumOfErrors() > 0) {
+                return response;
+            }
+
+            List<User> getAllUsers = userRepository.findAll();
+
+            for(User existingUser:getAllUsers) {
+                if (existingUser.getEmail().equals(user.getEmail())) {
+                    response.setMessage("This email already exists!");
+                    return response;
+                }
+            }
+
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+            user.setPassword(encoder.encode(user.getPassword()));
+            userRepository.save(user);
+            response.setMessage("User created successfully!");
+            return response;
 
         } catch (Exception e) {
             throw new Exception(e.toString());
--- src/main/java/io/github/aplaraujo/services/impl/IAuthService.java
+++ src/main/java/io/github/aplaraujo/services/impl/IAuthService.java
@@ -1,9 +1,12 @@
 package io.github.aplaraujo.services.impl;
 
+import io.github.aplaraujo.entities.User;
 import io.github.aplaraujo.services.models.dto.LoginDTO;
+import io.github.aplaraujo.services.models.dto.ResponseDTO;
 
 import java.util.HashMap;
 
 public interface IAuthService {
     public HashMap<String, String> login(LoginDTO dto) throws Exception;
+    public ResponseDTO register(User user) throws Exception;
 }
